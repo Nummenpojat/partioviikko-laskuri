07739a995500b182e1dd209a9956189a55da1c8a
Elias Mäkelä <eliasjmakela@protonmail.com>
2021-04-18 23:20:38 +0300

    Lisäsin TallenaKavija.ts tiedostoon juttuja

--- src/services/TallennaKavija.ts
+++ src/services/TallennaKavija.ts
@@ -1,5 +1,9 @@
 const TallennaKavija = (floor:string, timestamp: Date) => {
-    console.log(`Hello world from ${floor} at ${timestamp}`)
+    console.log(`Visitor registered in ${floor} at ${timestamp}`)
+    let url = "https://nummenpojat.fi/visitcounter"
+
+    fetch(url, {method: 'POST', body: JSON.stringify({ floor: floor, date: timestamp.toJSON() })})
 }
+
 
 export default TallennaKavija
